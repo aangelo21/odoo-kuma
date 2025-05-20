0d2d1d387e7083be384d2a3ff54efde218867a3b
Carlos <carlosperezsantana04@gmail.com>
2025-05-20 16:40:00 +0100

    Clase cursos creada

---
+++ addons/gestion_cursos/models/cursos.py
@@ -0,0 +1,31 @@
+from odoo import models, fields, api # type: ignore
+
+class Curso(models.Model):
+    _name = 'gestion_cursos.curso'
+    _description = 'gestion_cursos.curso'
+
+    nombre = fields.Text(string = 'Nombre')
+    descripcion = fields.Text(string = 'Descripción')
+    nivel = fields.Selection([
+        ('1', '1'),
+        ('2', '2'),
+        ('3', '3'),
+        ('4', '4')], string = 'Nivel')
+    duracion = fields.Integer(string = 'Duración')
+    modalidad = fields.Selection([
+        ('presencial', 'Presencial'),
+        ('semipresencial', 'Semipresencial'),
+        ('teleformación', 'Teleformación')], string = 'Modalidad')
+    localizacion = fields.Selection([
+    ('nacional', 'Nacional'),
+    ('teleformacion', 'Teleformación'),
+    ('gran_canaria', 'Gran Canaria'),
+    ('tenerife', 'Tenerife'),
+    ('lanzarote', 'Lanzarote'),
+    ('fuerteventura', 'Fuerteventura'),
+    ('la_palma', 'La Palma'),
+    ('la_gomera', 'La Gomera'),
+    ('el_hierro', 'El Hierro')], string='Localización')
+    unidades_didacticas = fields.Integer(string = 'Unidades didácticas')
+    fecha_inicio = fields.Date(string='Fecha Inicio')
+    fecha_fin = fields.Date(string='Fecha Fin')
