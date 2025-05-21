455a9619756ff53e56006d590df9e0999f24b6ee
Angelo <martinezglielmo@gmail.com>
2025-05-21 14:40:28 +0100

    Se mandan emails a los empleados al crear eventos nuevos y actualizar fechas de los existentes

--- addons/gestion_cursos/models/curso.py
+++ addons/gestion_cursos/models/curso.py
@@ -47,9 +47,36 @@ class Curso(models.Model):
                 'allday': True,
             })
             curso.id_evento_calendario = calendar_event
+    
+        employees = self.env['hr.employee'].search([])
+        
+        mail_template = {
+            'subject': f'Nuevo curso añadido al calendario: {curso.nombre}',
+            'body_html': f'''
+                <p>Se ha añadido un nuevo curso al calendario:</p>
+                <ul>
+                    <li><strong>Nombre:</strong> {curso.nombre}</li>
+                    <li><strong>Descripción:</strong> {curso.descripcion}</li>
+                    <li><strong>Categoría:</strong> {curso.id_categoria.nombre}</li>
+                    <li><strong>Familia profesional:</strong> {curso.id_familia_profesional.nombre}</li>
+                    <li><strong>Modalidad:</strong> {curso.modalidad}</li>
+                    <li><strong>Localización:</strong> {curso.localizacion}</li>
+                    <li><strong>Modalidad:</strong> {curso.modalidad}</li>
+                    <li><strong>Unidades didácticas:</strong> {curso.unidades_didacticas}</li>
+                    <li><strong>Duración:</strong> {curso.duracion}h</li>
+                    <li><strong>Nivel:</strong> {curso.nivel}</li>
+                    <li><strong>Fecha inicio:</strong> {curso.fecha_inicio}</li>
+                    <li><strong>Fecha fin:</strong> {curso.fecha_fin}</li>
+                </ul>
+            ''',
+            'email_from': self.env.user.email,
+            'email_to': ','.join(employees.mapped('work_email')),
+        }
+        self.env['mail.mail'].create(mail_template).send()
         return cursos
 
     def write(self, vals):
+        fecha_modificada = 'fecha_inicio' in vals or 'fecha_fin' in vals
         result = super().write(vals)
         for curso in self:
             if curso.id_evento_calendario:
@@ -60,6 +87,24 @@ class Curso(models.Model):
                     'stop': curso.fecha_fin or curso.fecha_inicio,
                     'allday': True,
                 })
+
+            if fecha_modificada:
+                employees = self.env['hr.employee'].search([])
+                
+                mail_template = {
+                    'subject': f'Actualización de fechas en el curso: {curso.nombre}',
+                    'body_html': f'''
+                        <p>Se han actualizado las fechas del curso en el calendario:</p>
+                        <ul>
+                            <li><strong>Nombre:</strong> {curso.nombre}</li>
+                            <li><strong>Nueva fecha inicio:</strong> {curso.fecha_inicio}</li>
+                            <li><strong>Nueva fecha fin:</strong> {curso.fecha_fin}</li>
+                        </ul>
+                    ''',
+                    'email_from': self.env.user.email,
+                    'email_to': ','.join(employees.mapped('work_email')),
+                }
+                self.env['mail.mail'].create(mail_template).send()
         return result
 
     def unlink(self):
