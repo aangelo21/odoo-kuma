916a32d7d4f1f1f4703ea56cce9f4ad742532b17
Angelo <martinezglielmo@gmail.com>
2025-05-28 12:28:30 +0100

    estilos🎨: ocultado acceso a bde y creacion de usuario desde login

--- addons/gestion_cursos/__manifest__.py
+++ addons/gestion_cursos/__manifest__.py
@@ -1,33 +1,19 @@
-# -*- coding: utf-8 -*-
 {
     'name': "Gestión Cursos",
-
     'summary': "Short (1 phrase/line) summary of the module's purpose",
-
     'description': """
 Long description of module's purpose
     """,
-
     'author': "My Company",
     'website': "https://www.yourcompany.com",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
     'category': 'Uncategorized',
-    'version': '0.1',
-
-    # any module necessary for this one to work correctly
-    'depends': ['base'],
-
-    # always loaded
+    'version': '0.1',    'depends': ['base'],
     'data': [
         'security/ir.model.access.csv',
         'views/calendario_vista.xml',
         'views/curso_vista.xml',
         'views/categoria_vista.xml',
         'views/familia_profesional_vista.xml',
-        # 'views/views.xml',
         'views/templates.xml',
     ],
     'assets': {
